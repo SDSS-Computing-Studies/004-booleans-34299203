1ccb5d0d84ed19788a2e19edf313a9d39d6caf39
19  5  2  1  19  20  9  1  14 <sebastianm880@deltalearns.ca>
2022-01-17 13:47:20 -0800

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -28,18 +28,13 @@ username = "admin"
 if username == x:
     print("access granted")
 else:
-    print("invalid user")
+    z = input("enter a password")   
 
-    z = input("enter a password")
+    
 password = "12345password"
 if password == z:
     print("access granted")
 else:
     print("access denied")
 
-
-
-
-
-
-
+    #if username 
